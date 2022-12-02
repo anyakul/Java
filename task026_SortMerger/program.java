ccc84592944225b61ce8557cbf48a81a45f69087
anyakulikova111@ <anyakulikova111@gmail.com>
2022-12-02 16:09:12 +0300

    lesson-3

--- task026_SortMerger/program.java
+++ task026_SortMerger/program.java
@@ -3,7 +3,7 @@
 package task026_SortMerger;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class program {
@@ -11,54 +11,60 @@ public class program {
         int count = 10;
         ArrayList<Integer> lst = GetRandomArray(count);
         System.out.println(lst);
-      //  mergeSort(lst);
+        List<Integer> sortLst = mergeSort(lst);
+        System.out.println(sortLst);
     }
 
     static ArrayList<Integer> GetRandomArray(int count) {
-        ArrayList<Integer> lst = new ArrayList<>(count);
+        ArrayList<Integer> arr = new ArrayList<>();
         Random rand = new Random();
 
         for (int i = 0; i < count; i++) {
-            int temp = rand.nextInt(20);
-            lst.add(temp);
+            arr.add(rand.nextInt(20));
         }
 
-        return lst;
-    }
-/*
-    public static int[] mergeSort(ArrayList<Integer> lst) {
-        int[] buffer1 = lst.copyOf(lst, lst.size());
-        int[] buffer2 = new int[lst.size()];
-        int[] result = System.arraycopy(buffer1, buffer2, 0, lst.size());
-        return result;
+        return arr;
     }
 
-    public static int[] mergeSortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
-        if (startIndex >= endIndex - 1) {
-            return buffer1;
+    public static List<Integer> mergeSort(List<Integer> lst) {
+        if (lst.size() <= 1) {
+            return lst;
+        }
+        List<Integer> left = new ArrayList<Integer>();
+        List<Integer> right = new ArrayList<Integer>();
+        for (int i = 0; i < lst.size(); i++) {
+            if (i < (lst.size()) / 2)
+                left.add(lst.get(i));
+            else {
+                right.add(lst.get(i));
+            }
+        }
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        return merge(left, right);
+    }
+
+    private static List<Integer> merge(List<Integer> l, List<Integer> r) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        while (l.size() > 0 && r.size() > 0) {
+            if (l.get(0) <= r.get(0)) {
+                result.add(l.get(0));
+                l.remove(0);
+            } else {
+                result.add(r.get(0));
+                r.remove(0);
+            }
+        }
+        while (l.size() > 0) {
+            result.add(l.get(0));
+            l.remove(0);
+        }
+        while (r.size() > 0) {
+            result.add(r.get(0));
+            r.remove(0);
         }
 
-        // уже отсортирован
-        int middle = startIndex + (endIndex - startIndex) / 2;
-        int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
-        int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
-
-        // слияние
-        int index1 = startIndex;
-        int index2 = middle;
-        int destIndex = startIndex;
-        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
-        while (index1 < middle && index2 < endIndex) {
-            result[destIndex++] = sorted1[index1] < sorted2[index2]
-                    ? sorted1[index1++]
-                    : sorted2[index2++];
-        }
-        while (index1 < middle) {
-            result[destIndex++] = sorted1[index1++];
-        }
-        while (index2 < endIndex) {
-            result[destIndex++] = sorted2[index2++];
-        }
         return result;
-    }*/
+    }
 }
