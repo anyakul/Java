d78b79675623414b67a9e7bdee83d7b7a15031e3
anyakulikova111@ <anyakulikova111@gmail.com>
2022-12-28 16:40:09 +0300

    lesson-10

--- task050_CalculatorGenericsTests/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import controllers.CalculatorController;
-
-public class Main {
-    public static void main(String[] args) {
-        CalculatorController calculatorController = new CalculatorController();
-        calculatorController.calc();
-    }
-}
